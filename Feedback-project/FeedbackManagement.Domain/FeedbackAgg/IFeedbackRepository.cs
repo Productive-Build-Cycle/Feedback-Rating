6f34d383b292b012749ac497bf1a8ded337a100f
Payam Karimi <pyamkrymy33@gmail.com>
2025-12-28 23:35:29 +0330

    Doman layer has been improved

---
+++ Feedback-project/FeedbackManagement.Domain/FeedbackAgg/IFeedbackRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedbackManagement.Domain.FeedbackAgg;
+
+public class IFeedbackRepository
+{
+
+}
---
+++ Feedback-project/FeedbackManagement.Domain/RatingAgg/IRatingRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeedbackManagement.Domain.RatingAgg;
+
+public interface IRatingRepository
+{
+}
