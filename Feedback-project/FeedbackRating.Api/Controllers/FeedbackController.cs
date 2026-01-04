d1313085b451a3e39431b465c63dd9e3943c816b
Payam Karimi <pyamkrymy33@gmail.com>
2026-01-05 00:32:57 +0330

    FeedbackRating.Api has been configured

---
+++ Feedback-project/FeedbackRating.Api/Controllers/FeedbackController.cs
@@ -0,0 +1,25 @@
+﻿using FeedbackManagement.Application.Contracts.Feedbacks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackRating.Api.Controllers
+{
+
+    [Route("[Controller]/[Action]")]
+
+    public class FeedbackController : Controller
+    {
+        private readonly IFeedbackApplication _feedbackApplication;
+
+        public FeedbackController(IFeedbackApplication feedbackApplication)
+        {
+            _feedbackApplication = feedbackApplication;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CreateFeedback command)
+        {
+            var result = _feedbackApplication.Create(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ Feedback-project/FeedbackRating.Api/Controllers/RatingController.cs
@@ -0,0 +1,41 @@
+﻿using FeedbackManagement.Application.Contracts.Ratings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedbackRating.Api.Controllers
+{
+
+
+
+    [Route("[controller]/[action]")]
+    public class RatingController : Controller
+    {
+        private readonly IRatingApplication _ratingApplication;
+
+        public RatingController(IRatingApplication ratingApplication)
+        {
+            _ratingApplication = ratingApplication;
+        }
+
+        [HttpPost]
+        public IActionResult Register([FromBody] RegisterRating command)
+        {
+            var result = _ratingApplication.Register(command);
+            return Ok(result);
+        }
+
+        [HttpGet("{targetId}/{targetType}")]
+        public IActionResult GetAverage(long targetId, int targetType)
+        {
+            var result = _ratingApplication.GetAverage(targetId, targetType);
+            return Ok(result);
+        }
+
+        [HttpGet("{targetId}/{targetType}")]
+        public IActionResult GetByTarget(long targetId, int targetType)
+        {
+            var list = _ratingApplication.GetByTarget(targetId, targetType);
+            return Ok(list);
+        }
+    }
+
+}
