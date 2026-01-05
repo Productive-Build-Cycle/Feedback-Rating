4578795ac8ef87c490fef7bc885880b96b5f9f9a
Payam Karimi <pyamkrymy33@gmail.com>
2026-01-05 12:26:54 +0330

    Documents has been Improved

--- Feedback-project/FeedbackRating.Api/Controllers/RatingController.cs
+++ Feedback-project/FeedbackRating.Api/Controllers/RatingController.cs
@@ -6,7 +6,7 @@ namespace FeedbackRating.Api.Controllers
 
 
 
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class RatingController : Controller
     {
         private readonly IRatingApplication _ratingApplication;
@@ -23,12 +23,16 @@ namespace FeedbackRating.Api.Controllers
             return Ok(result);
         }
 
+
+        //test(postman) for example https://localhost:7071/Rating/GetAverage/Average/3/1
         [HttpGet("Average/{targetId}/{targetType}")]
         public IActionResult GetAverage(long targetId, int targetType)
         {
             var result = _ratingApplication.GetAverage(targetId, targetType);
             return Ok(result);
         }
+
+        //test(postman) for example https://localhost:7071/Rating/GetByTarget/ByTarget/3/1
 
         [HttpGet("ByTarget/{targetId}/{targetType}")]
         public IActionResult GetByTarget(long targetId, int targetType)
